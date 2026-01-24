17d6fb82782581d353e7f05b966a319342a35d08
Ryan <Ryangerhardt1@gmail.com>
2026-01-24 13:29:46 -0800

    changed angular factor from 360 back to 0 and xbox

--- src/main/java/frc/robot/commands/XboxDrive.java
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -31,7 +31,7 @@ addRequirements(swerve);
   // XBox controller controls swerve modules
   @Override
   public void execute() {
-    swerve.control(xboxController.getLeftX(),-xboxController.getLeftY(),xboxController.getRightX());
+    swerve.control(xboxController.getRightX(),-xboxController.getRightY(),xboxController.getLeftX());
   }
 
   // Called once the command ends or is interrupted.
