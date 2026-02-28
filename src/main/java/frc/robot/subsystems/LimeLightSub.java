618109ef6a960ebe6fe51f417ebc85da15c55300
Ryan <Ryangerhardt1@gmail.com>
2026-02-28 10:18:35 -0800

    FORMATED LIMELIGHT SUB

--- src/main/java/frc/robot/subsystems/LimeLightSub.java
+++ src/main/java/frc/robot/subsystems/LimeLightSub.java
@@ -28,11 +28,11 @@ public class LimeLightSub extends SubsystemBase {
       Constants.LimeLight.VisionPID.LimelightVertical_I, Constants.LimeLight.VisionPID.LimelightVertical_D);
   private PIDController VisionRotationPID = new PIDController(Constants.LimeLight.VisionPID.LimelightRotation_P,
       Constants.LimeLight.VisionPID.LimelightRotation_I, Constants.LimeLight.VisionPID.LimelightRotation_D);
-  private double VisionHorizontalPIDSpeed = 0; 
+  private double VisionHorizontalPIDSpeed = 0;
   private double VisionVerticalPIDSpeed = 0;
-  private double VisionRotationPIDSpeed = 0; 
+  private double VisionRotationPIDSpeed = 0;
   private double desiredPose = 0;
-  
+
   /** Creates a new LimeLightSub. */
   public LimeLightSub(SwerveSub swerveSub) {
     m_swerveSub = swerveSub;
@@ -46,13 +46,13 @@ public class LimeLightSub extends SubsystemBase {
     );
 
     LimelightHelpers.setLEDMode_ForceOff("");
-    LimelightHelpers.SetFiducialIDFiltersOverride("", new int[]{1, 2, 3, 4}); // Only track these tag IDs
+    LimelightHelpers.SetFiducialIDFiltersOverride("", new int[] { 1, 2, 3, 4 }); // Only track these tag IDs
     SmartDashboard.putNumber("limelight Despired pose", 0);
   }
 
   @Override
   public void periodic() {
-        // Basic targeting data
+    // Basic targeting data
     double tx = LimelightHelpers.getTX(""); // Horizontal offset from crosshair to target in degrees
     double ty = LimelightHelpers.getTY(""); // Vertical offset from crosshair to target in degrees
     double ta = LimelightHelpers.getTA(""); // Target area (0% to 100% of image)
@@ -71,87 +71,93 @@ public class LimeLightSub extends SubsystemBase {
     // Add it to your pose estimator
     m_swerveSub.setVisionMeasurementStdDevs(VecBuilder.fill(.5, .5,
         9999999));
-    if(hasTarget){
-    m_swerveSub.addVisionMeasure(
-        limelightMeasurement.pose,
-        limelightMeasurement.timestampSeconds);
-    // This method will be called once per scheduler run
-    
-
-    //keep an eye on the "SwerveSub.publicSwerve.getYaw().getDegrees()" part may not be what we need
-    LimelightHelpers.SetRobotOrientation("limelight", m_swerveSub.getGyroDeg(),
-        0, 0, 0, 0, 0);
-    LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
-
-    // if our angular velocity is greater than 360 degrees per second, ignore vision
-    // updates
-    //keep an eye on this value it may or may not be what we need
-    if (Math.abs(m_swerveSub.getAngularVelocity()) > 360) {
-      doRejectUpdate = true;
-    }
-    if (mt2.tagCount == 0) {
-      doRejectUpdate = true;
-    }
-    if (!doRejectUpdate) {
-      m_swerveSub.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+    if (hasTarget) {
       m_swerveSub.addVisionMeasure(
-          mt2.pose,
-          mt2.timestampSeconds);
-     }
+          limelightMeasurement.pose,
+          limelightMeasurement.timestampSeconds);
+      // This method will be called once per scheduler run
 
-    VisionHorizontalPIDSpeed = VisionHorizontalPID.calculate(mt2.pose.getX());
-     VisionVerticalPIDSpeed = VisionVerticalPID.calculate(mt2.pose.getY());
-     VisionRotationPIDSpeed = VisionRotationPID.calculate(mt2.pose.getRotation().getDegrees());
+      // keep an eye on the "SwerveSub.publicSwerve.getYaw().getDegrees()" part may
+      // not be what we need
+      LimelightHelpers.SetRobotOrientation("limelight", m_swerveSub.getGyroDeg(),
+          0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
 
+      // if our angular velocity is greater than 360 degrees per second, ignore vision
+      // updates
+      // keep an eye on this value it may or may not be what we need
+      if (Math.abs(m_swerveSub.getAngularVelocity()) > 360) {
+        doRejectUpdate = true;
+      }
+      if (mt2.tagCount == 0) {
+        doRejectUpdate = true;
+      }
+      if (!doRejectUpdate) {
+        m_swerveSub.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+        m_swerveSub.addVisionMeasure(
+            mt2.pose,
+            mt2.timestampSeconds);
+      }
 
-     SmartDashboard.putNumber("LimeLight Pose X", mt2.pose.getX());
-     SmartDashboard.putNumber("LimeLight Pose Y", mt2.pose.getY());
-     SmartDashboard.putNumber("LimeLight Pose Theta", mt2.pose.getRotation().getDegrees());
-     SmartDashboard.putNumber("Limelight Tag ID", (int) LimelightHelpers.getFiducialID(""));
+      VisionHorizontalPIDSpeed = VisionHorizontalPID.calculate(mt2.pose.getX());
+      VisionVerticalPIDSpeed = VisionVerticalPID.calculate(mt2.pose.getY());
+      VisionRotationPIDSpeed = VisionRotationPID.calculate(mt2.pose.getRotation().getDegrees());
 
-     if((mt2.pose.getX() <= 0 ) && (mt2.pose.getY() == 0) && (mt2.pose.getRotation().getDegrees() == 0)) {
-      SmartDashboard.putString("Ready to Shoot", "FIRE!!!!");
-     }
-     else {
-      SmartDashboard.putString("Ready to Shoot", "HOLD");
-     }
-    }
-    else {
+      SmartDashboard.putNumber("LimeLight Pose X", mt2.pose.getX());
+      SmartDashboard.putNumber("LimeLight Pose Y", mt2.pose.getY());
+      SmartDashboard.putNumber("LimeLight Pose Theta", mt2.pose.getRotation().getDegrees());
+      SmartDashboard.putNumber("Limelight Tag ID", (int) LimelightHelpers.getFiducialID(""));
+
+      if ((mt2.pose.getX() <= 0) && (mt2.pose.getY() == 0) && (mt2.pose.getRotation().getDegrees() == 0)) {
+        SmartDashboard.putString("Ready to Shoot", "FIRE!!!!");
+      } else {
+        SmartDashboard.putString("Ready to Shoot", "HOLD");
+      }
+    } else {
       // deafault if no target detected
-     desiredPose = SmartDashboard.getNumber("limelight Despired pose", 9);
-     SmartDashboard.putNumber("LimeLight Pose X", -99);
-     SmartDashboard.putNumber("LimeLight Pose Y", -99);
-     SmartDashboard.putNumber("LimeLight Pose Theta", -99);
-     SmartDashboard.putNumber("Limelight pose wanted", desiredPose);
-     VisionHorizontalPIDSpeed = 0;
-     VisionVerticalPIDSpeed = 0;
-     VisionRotationPIDSpeed = 0;
+      desiredPose = SmartDashboard.getNumber("limelight Despired pose", 9);
+      SmartDashboard.putNumber("LimeLight Pose X", -99);
+      SmartDashboard.putNumber("LimeLight Pose Y", -99);
+      SmartDashboard.putNumber("LimeLight Pose Theta", -99);
+      SmartDashboard.putNumber("Limelight pose wanted", desiredPose);
+      VisionHorizontalPIDSpeed = 0;
+      VisionVerticalPIDSpeed = 0;
+      VisionRotationPIDSpeed = 0;
     }
   }
+
   public void addVisionHorizontalSetpoint(double setpoint) {
     VisionHorizontalPID.setSetpoint(setpoint);
   }
+
   public void addVisionVerticalSetpoint(double setpoint) {
     VisionVerticalPID.setSetpoint(setpoint);
   }
+
   public void addVisionRotationSetpoint(double setpoint) {
     VisionRotationPID.setSetpoint(setpoint);
   }
+
   public double CalculateVisionHorizontalPID(double setpoint) {
     return VisionHorizontalPIDSpeed;
   }
+
   public double CalculateVisionVerticalPID(double setpoint) {
     return VisionVerticalPIDSpeed;
   }
+
   public double CalculateVisionRotationPID(double setpoint) {
     return VisionRotationPIDSpeed;
   }
+
   public boolean hasTarget() {
     return LimelightHelpers.getTV("");
   }
+
   public boolean atSetPoint() {
     return VisionHorizontalPID.atSetpoint() && VisionVerticalPID.atSetpoint() && VisionRotationPID.atSetpoint();
   }
+
   public int getTargetID() {
     return (int) LimelightHelpers.getFiducialID("");
   }
